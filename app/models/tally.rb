501b062f8cf18d8566ba8525400e0e2ece9340e0
kruchkamgar <kruchkamgar@gmail.com>
2019-03-23 19:05:18 -0400

    DRY tally; added special teams' fields

--- app/models/tally.rb
+++ app/models/tally.rb
@@ -3,36 +3,55 @@ class Tally < ApplicationRecord
   # belongs_to :team
 # implement starting value of 0 for assists and goals and points
 
+  #  *1 - conceivably store these in table, with game_ids
+
   def tally_instances
+    plus_minus_values
+
+    Instance.columns
+    .select do |col|
+      col.type == :integer &&
+      col.default == "0" end
+    .each do |field|
+      map_reduce_and_total(field) end
+  end
+
+  def tally_derivatives
+    self.points =
+    self.goals + self.assists
+  end
+  def map_reduce_and_total(method, operation = :+)
+    total =
+    self.unit.instances
+    .map(& method)
+    .reduce(operation)
+
+    self.send(method) = total
+  end
+
+  def plus_minus_values
       plus_minus_values =
       self.unit.instances.
       map(&:plus_minus).compact
       self.plus_minus =
       plus_minus_values.reduce(:+) if plus_minus_values.any?
-
-      assists_values =
-      self.unit.instances.
-      map(&:assists)
-      self.assists =
-      assists_values.reduce(:+) if assists_values.any?
-
-      goals_values =
-      self.unit.instances.
-      map(&:goals)
-      self.goals =
-      goals_values.reduce(:+) if goals_values.any?
-
-      # if self.goals > 0 && self.assists > 0 then $interrupt = true end
-
-      self.points =
-      self.goals + self.assists
-
   end
 
-  # def map_reduce_this (item, method, operation = :+)
-  #   item.
-  #   map(& method).
-  #   reduce(operation)
+  # def assists_values
+  #     assists_values =
+  #     self.unit.instances.
+  #     map(&:assists)
+  #     self.assists =
+  #     assists_values.reduce(:+) if assists_values.any?
   # end
+  # def goals_values
+  #     goals_values =
+  #     self.unit.instances.
+  #     map(&:goals)
+  #     self.goals =
+  #     goals_values
+  #     .reduce(:+) if goals_values.any?
+  # end
+
 
 end
