0c764dbe21c636ee32807f660dae61780a351f60
kruchkamgar <kruchkamgar@gmail.com>
2019-11-24 13:25:55 -0700

    add table for LiveDataState

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_233642) do
+ActiveRecord::Schema.define(version: 2019_11_24_200005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,11 @@ ActiveRecord::Schema.define(version: 2019_04_17_233642) do
     t.integer "ppg", default: 0
     t.integer "shg", default: 0
     t.index ["unit_id"], name: "index_instances_on_unit_id"
+  end
+
+  create_table "live_data_states", force: :cascade do |t|
+    t.string "start_time"
+    t.integer "game_id"
   end
 
   create_table "log_entries", force: :cascade do |t|
