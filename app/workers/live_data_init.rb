54ccecb92d5c540f570efcd8d462909836e6aeff
kruchkamgar <kruchkamgar@gmail.com>
2019-12-06 15:20:06 -0700

    use class variable to store timeStamp

--- app/workers/live_data_init.rb
+++ app/workers/live_data_init.rb
@@ -10,6 +10,7 @@ class LiveDataInit
       date_hash, ts_instance["team"] ) # sets @game, including its game_id
     # add the roster ids to ts_instance
 
+    LiveData.time_stamp = ts_instance[:time_stamp]
     # check redis keys [by game_id] to see if game already scheduled for another team
     ReadNHLApis::schedule_live_data_job( ts_instance ) unless scheduled
 
