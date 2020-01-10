d50c4aa818313d0ee72fac7e439592ed5574ae30
kruchkamgar <kruchkamgar@gmail.com>
2020-01-09 18:29:17 -0700

    added 'TZ' spec

--- spec/lib/utilities_spec.rb
+++ spec/lib/utilities_spec.rb
@@ -11,12 +11,20 @@ describe Utilities do
       expect(time).to eq("04:33")
     end
 
-    it 'evaluates operations for hhmmss' do
+    it 'evaluates operations for \'yyyymmdd_hhmmss\'' do
       time = Utilities::TimeOperation.new(:+,
         [ { time: "20191117_052923", format: "yyyymmdd_hhmmss" },
           "1:33" ]).result
 
       expect(time).to eq("05:30:56")
+    end
+
+    it 'evaluates operations for \'TZ\'' do
+      time = Utilities::TimeOperation.new(:-,
+        [ { time: "2019-11-17T05:31:28Z", format: "TZ" },
+          "1:33" ]).result
+
+      expect(time).to eq("05:29:55")
     end
 
   end # TimeOperation
