12b1af4f0340483b75598b38597e26a3d76c2b55
kruchkamgar <kruchkamgar@gmail.com>
2020-03-04 22:50:09 -0700

    clean-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root to: 'units#index'
 
-  get 'teams/index'
-  get 'teams/show'
+  # get 'teams/index'
+  # get 'teams/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # resources :units, only: [:index, :show]
 
-  get '/units/index' => 'units#index'
   get '/units' => 'units#index'
   get '/units/:team_id' => 'units#show_units'
 
