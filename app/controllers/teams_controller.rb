40ff72d2b8f3f1409a91c5b729b23a873ab816e9
kruchkamgar <kruchkamgar@gmail.com>
2020-03-20 11:38:26 -0600

    allows range parameter

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,15 +5,24 @@ include Standings
     # teams = Team.last(5)
     #
     # render json: teams.to_json
-    render json: Hash[
-      power_scores: weighted_standings(),
-      schedule: get_schedule_data() ].to_json
   end
 
   def show
-    team_id = team_params[:id]
+    # team_id = team_params[:id]
+    #
+    # team = team.find(team_id)
+    # render json: team.to_json
+    range = games_params[:range].to_i
 
-    team = team.find(team_id)
-    render json: team.to_json
+    render json: Hash[
+      powerScores: weighted_standings(range),
+      schedule: get_schedule_data(Standings::DATE_NOW) ]
   end
+
+private
+
+  def games_params
+    params.permit(:range)
+  end
+
 end
