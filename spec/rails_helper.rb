ecc4113e0908080f766ab644d21d40e6f3073785
kruchkamgar <kruchkamgar@gmail.com>
2020-04-20 19:23:10 -0600

    require rspec/rails to rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-# require 'rspec/rails'
+require 'rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -56,7 +56,7 @@ RSpec.configure do |config|
   # config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
-  config.filter_rails_from_backtrace!
+  # config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
